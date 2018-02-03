0f53f70338c4e9032f82d0ae41831d9dfce8a43e
Jauch Pascal <pascal.jauch@hotmail.com>
2018-02-03 10:20:03 +0100

    toByte in Karte hinzugefügt

--- src/jass/Karte.java
+++ src/jass/Karte.java
@@ -7,16 +7,46 @@ public class Karte
 	private String name;
 	private String farbe;
 	
+	/**
+	 * 
+	 * @param f The Color of the Card
+	 * @param n The Name of the Card
+	 */
 	Karte(String f, String n)
 	{
 		farbe = f;
 		name = n;
 	}
 	
+	/**
+	 * @param k A ByteArray which represents a Card
+	 * 
+	 */
+	Karte(byte[] k)		//Generates a Card from a Byte-Array
+	{
+		String buf = new String(k);
+		StringBuffer karte = new StringBuffer(buf);
+		int n = 0;
+		this.wertigkeit = Integer.parseInt(karte.substring(0, n = karte.indexOf("-")));
+		this.punkte = Integer.parseInt(karte.substring(n+1, n = karte.indexOf("-", n+1)));
+		this. name = karte.substring(n+1, n = karte.indexOf("-", n+1));
+		this.farbe = karte.substring(n+1, karte.length());
+		
+	}
+	
+	/**
+	 * 
+	 * @param p The Number of Points (Value), this Card counts.
+	 */
 	public void setPunkte(int p)
 	{
 		punkte = p;
 	}
+	
+	/**
+	 * 
+	 * @return Returns the Number of Points (Value), this Card counts.
+	 */
 	public int getPunkte()
 	{
 		return punkte;
@@ -46,5 +76,16 @@ public class Karte
 
 	public String getName() {
 		return name;
+	}
+	
+	/**
+	 * 
+	 * @return Return a ByteArray that describes the Object, with which another card-Object can be constructed.
+	 */
+	public byte[] toByte()  ///Converts Card to a Byte-Array.
+	{
+		String buffer = new String();
+		buffer = (buffer + wertigkeit + "-" + punkte + "-" + name + "-" + farbe);
+		return buffer.getBytes();
 	}
 }
